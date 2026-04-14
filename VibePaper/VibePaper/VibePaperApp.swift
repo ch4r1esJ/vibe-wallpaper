cdb04d5531959e7476909659bf5c25f28784cae1
ch4r1esJ <charles.janjgava.git@gmail.com>
2026-04-14 23:58:37 +0400

    Initial Commit

---
+++ VibePaper/VibePaper/VibePaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibePaperApp.swift
+//  VibePaper
+//
+//  Created by Charles Janjgava on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct VibePaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
